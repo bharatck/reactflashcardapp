727bf61c7b8a98717ee45a913cce34e837f632b9
aartiborse03 <aartiborse03@gmail.com>
2018-06-04 10:59:39 +0530

    first commit

---
+++ src/Config/firebase/db_config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG={
+    apiKey: "AIzaSyD9XdihynEbiPCLSdoA5XM2FutdS_-oS-Q",
+    authDomain: "flashcardapp-b4259.firebaseapp.com",
+    databaseURL: "https://flashcardapp-b4259.firebaseio.com",
+    projectId: "flashcardapp-b4259",
+    storageBucket: "",
+    messagingSenderId: "456643387789"
+}
